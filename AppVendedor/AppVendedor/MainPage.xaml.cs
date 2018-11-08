0245701104c7da6e9c02763c7c4740d9654b9192
Robson Amorim <robson.amorim@lambda3.com.br>
2018-11-08 15:03:17 -0200

    inital commit

---
+++ AppVendedor/AppVendedor/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AppVendedor.Models;
+using AppVendedor.ViewModel;
+using Xamarin.Forms;
+
+namespace AppVendedor
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly MainPageViewModel _viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _viewModel = new MainPageViewModel();
+            BindingContext = _viewModel;
+        }
+
+        private void ListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            _viewModel.PedidoSelecionado = e.SelectedItem as Pedido;
+        }
+    }
+}
---
+++ AppVendedor/AppVendedor/Models/Pedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppVendedor.Models
+{
+    public class Pedido
+    {
+        public string KeyPedido { get; set; }
+        public string Cliente { get; set; }
+        public decimal Preco { get; set; }
+        public string Produto { get; set; }
+        public int IdVendedor { get; set; }
+    }
+}
